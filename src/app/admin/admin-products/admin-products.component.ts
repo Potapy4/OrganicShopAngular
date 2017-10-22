2aee9c174b8d07d90fcee28c34f10af5e58f4888
Potapy4 <dev.potapy4@gmail.com>
2017-10-22 10:54:26 +0300

    Search filter

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,19 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from '../../services/product.service';
+import { Subscription } from 'rxjs/Subscription';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnDestroy {
+  private subscription: Subscription;
+  products: Product[];
+  filteredProducts: any[];
 
   constructor(private productService: ProductService) {
-    this.products$ = productService.getAll();
+    this.subscription = productService.getAll()
+      .subscribe(products => this.filteredProducts = this.products = products);
   }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  public filter(query: string) {
+    this.filteredProducts = (query) ?
+      this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+      this.products;
   }
 
 }
