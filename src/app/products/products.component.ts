675a8f8f05eea9c54c8583a49721ce226e22f30a
Potapy4 <dev.potapy4@gmail.com>
2017-10-24 16:23:51 +0300

    Done with Shopping-Cart

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -2,49 +2,45 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../models/product';
-import 'rxjs/add/operator/switchMap';
 import { ShoppingCartService } from '../services/shopping-cart.service';
-import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
+import { ShoppingCart } from '../models/shopping-cart';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit, OnDestroy {
+export class ProductsComponent implements OnInit {
   products: Product[] = [];
   filteredProducts: Product[] = [];
   category: string;
-  cart: any;
-  subscription: Subscription;
+  cart$: Observable<ShoppingCart>;
 
   constructor(
     private route: ActivatedRoute,
     private productService: ProductService,
-    private cartService: ShoppingCartService) {
-
-    productService.getAll().switchMap(products => {
-      this.products = products;
-      return route.queryParamMap;
-    }).subscribe(params => {
-      this.category = params.get('category');
-      this.filter();
-    });
-
-  }
+    private cartService: ShoppingCartService) { }
 
   async ngOnInit() {
-    this.subscription = (await this.cartService.getCart()).subscribe(cart => this.cart = cart);
+    this.cart$ = await this.cartService.getCart();
+    this.populateProducts();
   }
 
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
+  private populateProducts() {
+    this.productService.getAll().switchMap(products => {
+      this.products = products;
+      return this.route.queryParamMap;
+    }).subscribe(params => {
+      this.category = params.get('category');
+      this.applyFilter();
+    });
   }
 
-  private filter() {
+  private applyFilter() {
     this.filteredProducts = (this.category) ?
       this.products.filter(p => p.category === this.category) :
       this.products;
   }
-
 }
--- src/app/services/shopping-cart.service.ts
+++ src/app/services/shopping-cart.service.ts
@@ -11,15 +11,28 @@ export class ShoppingCartService {
 
   constructor(private db: AngularFireDatabase) { }
 
+  public async getCart(): Promise<Observable<ShoppingCart>> {
+    let cartId = await this.getOrCreateCartId();
+    return this.db.object('/shopping-carts/' + cartId).map(x => new ShoppingCart(x.items));
+  }
+
+  public async addToCart(product: Product) {
+    this.updateItem(product, 1);
+  }
+
+  public async removeFromCart(product: Product) {
+    this.updateItem(product, -1);
+  }
+
+  public async clearCart() {
+    let cartId = await this.getOrCreateCartId();
+    this.db.object(`/shopping-carts/${cartId}/items`).remove();
+  }
+
   private create() {
     return this.db.list('/shopping-carts').push({
       dateCreated: new Date().getTime()
     });
-  }
-
-  public async getCart(): Promise<Observable<ShoppingCart>> {
-    let cartId = await this.getOrCreateCartId();
-    return this.db.object('/shopping-carts/' + cartId).map(x => new ShoppingCart(x.items));
   }
 
   private getItem(cartId: string, productId: string) {
@@ -39,20 +52,17 @@ export class ShoppingCartService {
     return result.key;
   }
 
-  public async addToCart(product: Product) {
-    this.updateItem(product, 1);
-  }
-
-  public async removeFromCart(product: Product) {
-    this.updateItem(product, -1);
-  }
-
   private async updateItem(product: Product, change: number) {
     let cartId = await this.getOrCreateCartId();
     let item$ = this.getItem(cartId, product.$key);
 
     item$.take(1).subscribe(item => {
-      item$.update({ title: product.title, imageUrl: product.imageUrl, price: product.price, quantity: (item.quantity || 0) + change });
+      let quantity = (item.quantity || 0) + change;
+      if (quantity === 0) {
+        item$.remove();
+      } else {
+        item$.update({ title: product.title, imageUrl: product.imageUrl, price: product.price, quantity: quantity });
+      }
     });
   }
 }
